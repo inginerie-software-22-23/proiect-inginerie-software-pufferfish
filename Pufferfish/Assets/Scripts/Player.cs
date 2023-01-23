e0092bc57fb73c2e149efa9e0275c0136299a0b7
ciurix <69077153+ciurix@users.noreply.github.com>
2023-01-23 12:08:58 +0200

    Merg border-urile, scuze beje ca am dat push pe main

--- Pufferfish/Assets/Scripts/Player.cs
+++ Pufferfish/Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-[RequireComponent(typeof(BoxCollider2D))]
+//[RequireComponent(typeof(BoxCollider2D))]
 
 public class Player : MonoBehaviour
 {
@@ -12,7 +12,7 @@ public class Player : MonoBehaviour
     private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
     private float playerx, playerz, playery;
-    //private RaycastHit2D hit;
+    private RaycastHit2D hit;
 
 
 
@@ -41,9 +41,27 @@ public class Player : MonoBehaviour
         {
             transform.localScale = new Vector3(-Math.Abs(playerx), playery, playerz);
         }
-        transform.Translate(moveDelta * Time.deltaTime);    
 
+        //moving in y axis
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask( "Blocking"));
+        if(hit.collider == null)
+        {
+
+            Debug.Log("not border");
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+        else
+        {
+
+            Debug.Log("border");
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask( "Blocking"));
+        if (hit.collider == null)
+        {
+
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
         //Debug.Log(x);
-        //Debug.Log(y);
     }
 }
